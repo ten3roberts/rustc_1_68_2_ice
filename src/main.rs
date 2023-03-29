9c3af0cdf13439063d869a365df8ae561b464657
Tei Leelo Roberts <ten3roberts@gmail.com>
2023-03-29 23:42:53 +0200

    feat: minimize

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
 use core::marker::PhantomData;
 
-struct Component<T> {
-    marker: PhantomData<T>,
-}
+struct Component<T>(PhantomData<T>);
 
-struct Wrapper<T, V> {
-    component: T,
-    value: V,
-}
+struct Wrapper<T>(T);
 
-impl<'a, 'w, Q> AsBorrow<'a> for QueryData<'w, Q>
-where
-    Q: 'static,
-    QueryBorrow<'w, Q>: 'a,
-{
-    type Borrowed = QueryBorrow<'w, Q>;
-}
-
-pub trait SystemData<'a> {
-    type Value;
-}
-
-impl<'a, Q> SystemData<'a> for Q
-where
-    Q: 'static,
-{
-    type Value = QueryData<'a, Q>;
+impl<'a, Q: 'a> AsBorrow<'a> for Q {
+    type Borrowed = QueryBorrow<'a, Q>;
 }
 
 pub struct QueryData<'a, Q> {
@@ -43,33 +23,27 @@ pub trait AsBorrow<'a> {
 
 /// A callable function with 'self lifetime
 /// Is not reproducible with Fn trait
-trait SystemFn<'this, Args, Ret> {
+trait SystemFn<'this, Args> {
     // fn execute(&'this mut self, args: Args) -> Ret;
 }
 
 pub fn build<Args, Func>(data: Args, func: Func)
 where
-    Args: for<'a> SystemData<'a> + 'static,
-    Func: for<'this, 'a> SystemFn<'this, <Args as SystemData<'a>>::Value, ()>,
+    Func: for<'this, 'a> SystemFn<'this, Args>,
 {
 }
 
-impl<'this, Func, Ret, A> SystemFn<'this, A, Ret> for Func
+impl<'this, Func, A> SystemFn<'this, A> for Func
 where
     for<'x> A: AsBorrow<'x>,
-    for<'x> Func: Fn(<A as AsBorrow<'x>>::Borrowed) -> Ret,
+    for<'x> Func: Fn(<A as AsBorrow<'x>>::Borrowed),
 {
 }
 
 fn main() {
-    let component: Component<i32> = Component {
-        marker: PhantomData,
-    };
+    let component: Component<i32> = Component(PhantomData);
 
-    let query = Wrapper {
-        component,
-        value: 0,
-    };
+    let query = Wrapper(component);
 
     // Component instead of the actual Wrapper<Component<i32>, i32>>
     let system = build(query, |query: QueryBorrow<Component<i32>>| {});
